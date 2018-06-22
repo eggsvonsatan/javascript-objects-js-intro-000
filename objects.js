706251744a340bc846d42d447c6feb7387fe2582
eggsvonsatan <elbadry.d@gmail.com>
2018-06-22 02:19:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {'Phil Ochs' : 'closer'
 };
 function updatePlaylist(playlist, artistName , songTitle) {
@@ -7,3 +8,11 @@ function removeFromPlaylist(playlist, artistName){
   var newPlaylist =  Object.assign({},playlist);
   delete playlist[artistName];
   return newPlaylist}
+=======
+var playlist = {
+  nine inch nails: closer
+};
+function updatePlaylist(playlist, nine inch nails, closer) {
+  return Object.assign({}, playlist, {[nine inch nails]: closer});
+}
+>>>>>>> d6b7ca170a32b05b7f794dec5810d4e32e8db5f0
